798ab1ccedab298f933fc42eb48614696b023f83
chihiroKoba0329 <kobako2023@gmail.com>
2023-05-24 19:59:51 +0900

    修正と追加

--- javascript-basic/js/function.js
+++ javascript-basic/js/function.js
@@ -20,3 +20,21 @@ const addTwoArguments = (prices,shippingFee) => {
 }
 addTwoArguments(1200,600);
 
+//戻り値を返す関数を定義する
+const isPurchased = () =>{
+	if (商品が購入されているならば){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+//戻り値を返す関数を条件式に使う（trueであれば処理が実行される）
+if (isPurchased()) {
+	console.log(''購入済みです);
+}
+//与えられた引数numを2倍にし、その値を戻り値として返す関数を定義
+const double = (num) => {
+	return num * 2;
+}
+console.log(double(30));
---
+++ javascript-basic/js/function2.js
@@ -0,0 +1,11 @@
+//与えられた引数numを2倍にし、その値を戻り値として返す関数を定義
+const double = (num) => {
+	return num * 2;
+}
+//関数の戻り値を出力する
+console.log(double(30));
+
+//定数を宣言する
+const userName = '侍太郎'
+//定数の値を出力する
+console.log(userName);
