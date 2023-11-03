44eb7e473617eb78d748447d8bcaa54b3ac85f72
chihiroKoba0329 <kobako2023@gmail.com>
2023-11-03 10:41:48 +0900

    修正と追加

---
+++ efureji/js/buger.js
@@ -0,0 +1,16 @@
+
+const btn = document.getElementById("btn");
+const menu = document.getElementById("menu");
+const links = document.querySelectorAll("#menu a");
+
+btn.addEventListener("click", () => {
+  btn.classList.toggle("on");
+  menu.classList.toggle("on");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    btn.classList.toggle("on");
+    menu.classList.toggle("on");
+  });
+});
